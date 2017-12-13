83cb5d985455b66ad15969cd640fe13427b01e3f
sashamichelle <sashaavilescalderon1999@gmail.com>
2017-12-13 15:30:14 -0300

    mi reto de codigo aun por actualizar solo con primera y segunda pantalla

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+/*función para que desaparezca la pantalla de inicio*/
+
+$(function(){
+        $(".caja-1").fadeOut(1500);
+    },3000);
+
+/*función para que aparezca mi segunda parte*/
+$(function(){
+        $(".caja-2").fadeIn(1500);
+    },3000);
+});
