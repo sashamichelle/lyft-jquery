4810e1959a6cdd34a12346b38658971008830f97
sashamichelle <sashaavilescalderon1999@gmail.com>
2017-12-14 11:14:23 -0300

    añadiendo funcion para desactivar boton next y validar numeros

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,4 +10,29 @@ $(function(){
 $(function(){
         $(".caja-2").fadeIn(1500);
     },3000);
+
+/*funcion para tomar el valor del select del modal*/
+$('select#select').on('change',function(){
+	var option= $(this).val();
+	alert("Eres de " + option);
+});
+
+/* funcion para ingresar numero de telefono, activar y desactivar el boton*/
+ $('#text').keyup(function(){
+        if($(this).val().length === 10){
+            $('#siguiente').removeClass('disabled');
+        }if($(this).val().length < 10){
+            $('#siguiente').addClass('disabled');
+        }if($(this).val().length > 10){
+            $('#siguiente').addClass('disabled');
+            $('#text').prop('disabled',true);/*detener el input*/
+            $('#text').prop('disabled',false);/*volver a activar*/
+        }if (isNaN($(this).val())){
+            $('#alert').text('Sólo Números');
+            $('#next').addClass('disabled');
+        }else{
+            $('#alert').text('');
+        }
+        return false;
+    });
 });
